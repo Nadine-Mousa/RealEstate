c2095e77d75f9130bc75cfefa2086e3bd3f17fd4
Nadine Mousa <nadinemousa1@gmail.com>
2025-04-22 17:56:48 +0200

    first commit

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-property-card',
+    templateUrl: 'property-card.component.html',
+    styleUrls: ['property-card.component.css']
+})
+
+export class PropertyCardComponent  {
+    Property : any = {
+        Id: 101,
+        Name: "Modern Apartment in Downtown",
+        Type: "Apartment",
+        Price: 350000,
+        Location: "123 Nile Street, Zamalek, Cairo",
+        Bedrooms: 3,
+        Bathrooms: 2,
+        Area: 200 
+        
+    }
+}
