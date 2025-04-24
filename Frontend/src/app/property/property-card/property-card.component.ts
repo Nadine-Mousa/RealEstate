2822c95cf2bbf674803445646150c45456e6d07a
Nadine Mousa <nadinemousa1@gmail.com>
2025-04-24 18:52:48 +0200

    Property List and Card

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-property-card',
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PropertyCardComponent  {
-    Property : any = {
-        Id: 101,
-        Name: "Modern Apartment in Downtown",
-        Type: "Apartment",
-        Price: 350000,
-        Location: "123 Nile Street, Zamalek, Cairo",
-        Bedrooms: 3,
-        Bathrooms: 2,
-        Area: 200 
-        
-    }
+    @Input () Property : any;
+    // property : any = {
+    //     Id: 101,
+    //     Name: "Modern Apartment in Downtown",
+    //     Type: "Apartment",
+    //     Price: 350000,
+    //     Location: "123 Nile Street, Zamalek, Cairo",
+    //     Bedrooms: 3,
+    //     Bathrooms: 2,
+    //     Area: 200 
+    // }
 }
